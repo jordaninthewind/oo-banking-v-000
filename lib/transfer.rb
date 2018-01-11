b75b0d6431d20f54f3d676b0c9ff552fd5728712
Jordan Kline <jordan.kline@gmail.com>
2018-01-11 02:04:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ attr_accessor :sender, :receiver, :amount, :status
   end
 
   def execute_transaction
-    if self.valid?
+    if self.valid? && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
